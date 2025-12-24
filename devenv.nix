dce39121deea36c05aa9135224bd8c57dd92e529
Dax Harris <git@dax.gay>
2025-12-24 14:50:17 -0500

    setup environment

---
+++ devenv.nix
@@ -0,0 +1,40 @@
+{
+    pkgs,
+    lib,
+    config,
+    inputs,
+    ...
+}:
+
+{
+    packages = [
+        pkgs.git
+        pkgs.diesel-cli
+    ];
+    languages = {
+        rust.enable = true;
+        typescript.enable = true;
+        javascript = {
+            enable = true;
+            pnpm = {
+                enable = true;
+                install.enable = true;
+            };
+        };
+    };
+
+    services.postgres = {
+        enable = true;
+        initialScript = ''
+            CREATE ROLE abyssal WITH LOGIN PASSWORD 'abyssal' CREATEDB;
+        '';
+        listen_addresses = "127.0.0.1";
+        port = 5432;
+    };
+
+    env = {
+        ABYSSAL_DB__BACKEND = "postgres";
+        ABYSSAL_DB__URL = "postgresql://abyssal:abyssal@localhost:5432/abyssal";
+        DATABASE_URL = config.env.ABYSSAL_DB__URL;
+    };
+}
